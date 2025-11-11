8df1e2107d03ddd1a3fddd3ddf977ff3f67c5d10
isabelle-yi <yixiuran@163.com>
2025-11-12 00:16:49 +0800

    lesson4 task3.js有改动

--- lesson4/task3.js
+++ lesson4/task3.js
@@ -3,14 +3,14 @@ let age=prompt("请输入你的年龄：");
 console.log("你的年龄是："+age);
 if(age<18)
 {
-    console.log("未成年");
+    alert("未成年");
 }else if(age>=18&&age<30)
 {
-    console.log("青年");
+    alert("青年");
 }else if(age>=30&&age<60)
 {
-    console.log("中年");
+    alert("中年");
 }else if(age>=60)
 {
-    console.log("老年");
+    alert("老年");
 }
